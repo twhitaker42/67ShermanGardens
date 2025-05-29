c4c63fbf614b6d80316f9af31c1893abce145a32
WHITAKER <twhitaker42@gmail.com>
2025-05-29 18:35:52 -0400

    more images

--- src/data/initialPlants.ts
+++ src/data/initialPlants.ts
@@ -3,6 +3,15 @@ import { Plant } from '../types/Plant';
 export const initialPlants: Plant[] = [
   {
     id: '1',
+    commonName: 'Blue Columbine',
+    scientificName: 'Aquilegia coerulea',
+    imageUrl: 'https://commons.wikimedia.org/wiki/File:Aquilegia_coerulea_-_Flickr_-_aspidoscelis.jpg',
+    wikiUrl: 'https://en.wikipedia.org/wiki/Aquilegia_coerulea',
+    plantType: 'Wildflower',
+    isNativeToCO: true,
+  },
+  {
+    id: '2',
     commonName: 'Sheep Fescue',
     scientificName: 'Festuca ovina',
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Lolium.perenne.2.jpg',
@@ -11,16 +20,16 @@ export const initialPlants: Plant[] = [
     isNativeToCO: false,
   },
   {
-    id: '2',
+    id: '3',
     commonName: 'Blue Grama',
     scientificName: 'Bouteloua gracilis',
-    imageUrl: 'https://en.wikipedia.org/wiki/Bouteloua_gracilis#/media/File:Blue_grama_Bouteloua_gracilis_MN_2007.JPG',
+    imageUrl: 'https://commons.wikimedia.org/wiki/File:Bouteloua_gracilis_-_blue_grama_-_Flickr_-_Matt_Lavin_%285%29.jpg',
     wikiUrl: 'https://en.wikipedia.org/wiki/Bouteloua_gracilis',
     plantType: 'Grass',
     isNativeToCO: true,
   },
   {
-    id: '3',
+    id: '4',
     commonName: 'Basil',
     scientificName: 'Ocimum basilicum',
     imageUrl: 'https://images.pexels.com/photos/906065/pexels-photo-906065.jpeg',
